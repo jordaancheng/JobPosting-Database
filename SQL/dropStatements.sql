b616dd84a411c3fb7f1a80e6988606cce6ace6a3
Jordan <p8h0b@ugrad.cs.ubc.ca>
2019-03-27 16:59:32 -0700

    added drop table statements script

---
+++ SQL/dropStatements.sql
@@ -0,0 +1,18 @@
+USE job_posting;
+
+DROP TABLE Have_Reference;
+DROP TABLE ApplyTo_Application;
+DROP TABLE Create_Application;
+DROP TABLE Participate_Interview1;
+DROP TABLE Participate_Interview2;
+DROP TABLE For_Interview1;
+DROP TABLE For_Interview2;
+DROP TABLE Post;
+DROP TABLE Receive;
+DROP TABLE Applicant;
+DROP TABLE Employer1;
+DROP TABLE Employer2;
+DROP TABLE For_JobOffer;
+DROP TABLE JobPosting1;
+DROP TABLE JobPosting3;
+DROP TABLE JobPosting4;
